c17df515b9e20b5c84e0bf635aee400dc922c6de
Jimin Hwang <wlalssp@gmail.com>
2025-07-23 16:09:28 +0900

    Chore. Text Style 추가
    Why: 공통된 폰트 사이즈 및 굵기 사용을 위해
    How: 텍스트 스타일 명시 및 예제페이지 생성

--- Packages/DesignSystem/Sources/DesignSystem/Foundation/DSFont.swift
+++ Packages/DesignSystem/Sources/DesignSystem/Foundation/DSFont.swift
@@ -6,39 +6,79 @@ public extension DesignSystem {
     /// 사용자의 기기 설정에 따라 크기가 자동으로 조절됩니다.
     enum Font {
         // Pretendard 폰트 이름을 정확하게 사용해야 합니다.
+        private static let black = "Pretendard-Black"
         private static let bold = "Pretendard-Bold"
-        private static let regular = "Pretendard-Regular"
-        private static let medium = "Pretendard-Medium"
+        private static let extraBold = "Pretendard-ExtraBold"
+        private static let extraLight = "Pretendard-ExtraLight"
         private static let light = "Pretendard-Light"
-        
-        /// Bold, 28pt (at large size)
-        public static var title1: SwiftUI.Font {
-            .custom(bold, size: 28, relativeTo: .largeTitle)
+        private static let medium = "Pretendard-Medium"
+        private static let regular = "Pretendard-Regular"
+        private static let semiBold = "Pretendard-SemiBold"
+        private static let thin = "Pretendard-Thin"
+
+        // MARK: - Title 1
+        public enum Title1 {
+            public static var semibold: SwiftUI.Font {
+                .custom(Font.semiBold, size: 24, relativeTo: .largeTitle)
+            }
         }
         
-        /// Bold, 22pt (at large size)
-        public static var title2: SwiftUI.Font {
-            .custom(bold, size: 22, relativeTo: .title2)
+        // MARK: - Title 2
+        public enum Title2 {
+            public static var semibold: SwiftUI.Font {
+                .custom(Font.semiBold, size: 18, relativeTo: .title)
+            }
+            
+            public static var bold: SwiftUI.Font {
+                .custom(Font.bold, size: 18, relativeTo: .title)
+            }
         }
         
-        /// Bold, 17pt (at large size)
-        public static var headline: SwiftUI.Font {
-            .custom(bold, size: 17, relativeTo: .headline)
+        // MARK: - Headline
+        public enum Headline {
+            public static var semibold: SwiftUI.Font {
+                .custom(Font.semiBold, size: 14, relativeTo: .headline)
+            }
+            
+            public static var medium: SwiftUI.Font {
+                .custom(Font.medium, size: 14, relativeTo: .headline)
+            }
+            
+            public static var regular: SwiftUI.Font {
+                .custom(Font.regular, size: 14, relativeTo: .headline)
+            }
         }
         
-        /// Regular, 17pt (at large size)
-        public static var body: SwiftUI.Font {
-            .custom(regular, size: 17, relativeTo: .body)
+        // MARK: - Title 3
+        public enum Title3 {
+            public static var semibold: SwiftUI.Font {
+                .custom(Font.semiBold, size: 16, relativeTo: .title3)
+            }
+            
+            public static var medium: SwiftUI.Font {
+                .custom(Font.medium, size: 16, relativeTo: .title3)
+            }
         }
         
-        /// Regular, 15pt (at large size)
-        public static var callout: SwiftUI.Font {
-            .custom(regular, size: 15, relativeTo: .callout)
+        // MARK: - Large Title
+        public enum LargeTitle {
+            public static var semibold: SwiftUI.Font {
+                .custom(Font.semiBold, size: 20, relativeTo: .largeTitle)
+            }
         }
         
-        /// Regular, 12pt (at large size)
-        public static var caption: SwiftUI.Font {
-            .custom(regular, size: 12, relativeTo: .caption)
+        // MARK: - Body
+        public enum Body {
+            public static var regular: SwiftUI.Font {
+                .custom(Font.regular, size: 12, relativeTo: .body)
+            }
+        }
+        
+        // MARK: - Navigation Title
+        public enum NavigationTitle {
+            public static var bold: SwiftUI.Font {
+                .custom(Font.bold, size: 28, relativeTo: .largeTitle)
+            }
         }
         
         /// **[예외 처리용]** Dynamic Type을 사용하지 않는 고정 크기 폰트를 반환합니다.
@@ -54,6 +94,8 @@ public extension DesignSystem {
                 fontName = light
             case .medium:
                 fontName = medium
+            case .semibold:
+                fontName = semiBold
             default:
                 fontName = regular
             }
