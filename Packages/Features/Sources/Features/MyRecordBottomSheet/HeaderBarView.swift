de1ce41542862fd2d8f3212c66e7439d756f1331
RoastedJM <wlalssp@gmail.com>
2025-07-29 14:17:44 +0900

    Design. MyRecord 수정
    Why: Figma 디자인 반영
    How: Padding 수정, 리스트를 LazyVStack으로 변경

--- Packages/Features/Sources/Features/MyRecordBottomSheet/HeaderBarView.swift
+++ Packages/Features/Sources/Features/MyRecordBottomSheet/HeaderBarView.swift
@@ -29,28 +29,6 @@ struct HeaderBarView: View {
             
             HomeButton(action: onCloseTapped)
 
-            
-          /*  Button(action: onCloseTapped) {
-                ZStack {
-                    EllipticalGradient(
-                        stops: [
-                            .init(color: Color(red: 0.96, green: 0.98, blue: 1), location: 0.00),
-                            .init(color: Color(red: 0.86, green: 0.92, blue: 1), location: 1.00),
-                        ],
-                        center: .center
-                    )
-                    .frame(width: 36, height: 36)
-                    .cornerRadius(99)
-                    
-                    DesignSystemAssets.image(named: "flowerLogo")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 36, height: 36)
-                        .clipShape(Circle())
-                        .opacity(0.5)
-                }
-                .buttonStyle(.plain)
-            } */
         }
         .padding(.bottom, 8)
     }
